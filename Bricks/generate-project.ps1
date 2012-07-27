f02ea47f00ca466c4aca5e7c00252ffee74896c5
Dave Cowart <davecowart@gmail.com>
2012-07-27 13:48:38 -0500

    project clone working

--- Bricks/generate-project.ps1
+++ Bricks/generate-project.ps1
@@ -3,26 +3,22 @@ function brick-create-project([string]$name) {
 	Write-Host "1: MVC3"
 	Write-Host "2: N2CMS (MVC3)"
 	Write-Host
-	$type = Read-Host "Pick a project type"
+	$type = Read-Host "Pick a project type (Enter for 1)"
 
-	if ($type -eq 1) {
-		Write-Host "Creating MVC3 Project"
-		$repo = "git@github.com:davecowart/mason-mvc3.git"
-	} elseif ($type -eq 2) {
+	if ($type -eq 2) {
 		Write-Host "Creating N2CMS (MVC3) Project"
 		$repo = "git@github.com:davecowart/mason-n2cms.git"
 	} else {
-		Write-Host
-		Write-Host "Invalid option" -foregroundcolor red
-		. brick-create-project($name)
+		Write-Host "Creating MVC3 Project"
+		$repo = "git@github.com:davecowart/mason-mvc3.git"
 	}
 
 	mkdir $name
 	cd $name
-	git clone $repo
-	#rm .git
-	#git init
-	#git add .
-	#git commit -m "init"
-	#cd ..
+	git clone $repo .
+	rm -r -force .git
+	git init
+	git add .
+	git commit -m "init"
+	cd ..
 }
